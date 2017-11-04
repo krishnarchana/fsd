0148b8b3672d987121fd0d9b4d392a1f073ac2ec
gopikrishna <gopikrishna.e84@gmail.com>
2017-11-04 19:58:43 +0530

    second commit

--- FSDUI/src/app/app.module.ts
+++ FSDUI/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing';
+import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login-routing';
+import { ContactComponent } from './contact.component';
+import { ContactRoutingModule } from './contact-routing';
+import { AboutComponent } from './about.component';
+import { AboutRoutingModule } from './about-routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    LoginRoutingModule,
+    ContactRoutingModule,
+    AboutRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
