ff37551989d43213a55691f100177ad3964ed5e7
gopikrishna <gopikrishna.e84@gmail.com>
2017-11-06 00:00:06 +0530

    third commit

--- FSDUI/src/app/app.component.ts
+++ FSDUI/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+//import { Location } from '@angular/common';
+//import { Router } from '@angular/router';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+//  route: string;
+//  visability;
+//  constructor(location: Location, router: Router) {
+//    router.events.subscribe((val) => {
+//      if (location.path() === 'home' || location.path() === 'login') {
+//        this.route = location.path();
+//        this.visability = true;
+//      } else {
+//        this.route = 'login';
+//        this.visability = false;
+//      }
+//    });
+//  }
 }
---
+++ FSDUI/src/app/register.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit, Input} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: './register.component.html'
+})
+export class RegisterComponent {
+
+}
+
